e678007c57a54d3073eddd4b38aa0ef8063aaeba
Grad Laurentiu <gradlaur@yahoo.com>
2024-10-09 22:54:02 +0300

    Add files via upload

---
+++ HYGRO_handler.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity HYGRO_handler is
+--    Port(
+    
+--    );
+end HYGRO_handler;
+
+architecture Behavioral of HYGRO_handler is
+
+begin
+
+
+end Behavioral;
